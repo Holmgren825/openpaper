f89b9712f071bbbdc1177adc4a5eec8098766dd0
Erik Holmgren <erikholmgren24@gmail.com>
2020-11-15 20:02:39 +0100

    Added submit page to server.

--- openServer.py
+++ openServer.py
@@ -2,13 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return(render_template('main.html'))
 
+
 @app.route('/read/')
 def read():
     return(render_template('read.html'))
+
+
+@app.route('/submit/')
+def submit():
+    return(render_template('submit.html'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
