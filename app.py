a2f068645d1d76d85a23d488e1af2d96b9b3f749
Erik Holmgren <erikholmgren24@gmail.com>
2022-02-26 16:44:09 +0100

    Remove old stuff

--- app.py
+++
@@ -1,8 +0,0 @@
-from openpaper import app, db
-from openpaper import models
-from openpaper.models import User, Post
-
-
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post}
