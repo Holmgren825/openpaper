4c8f1099d2d487a97de85b97f7427cfe32daddf4
Erik Holmgren <erikholmgren24@gmail.com>
2022-03-03 18:09:05 +0100

    Export GUN as well.

--- src/initGun.js
+++ src/initGun.js
@@ -1,10 +1,10 @@
-import Gun from "gun/gun";
+import GUN from "gun";
 import Sea from 'gun/sea';
 
 let peers;
 
-window.sea = Sea;
-window.gun = window.sea.Gun;
+//window.sea = Sea;
+//window.gun = window.sea.Gun;
 
 if (process.env.NODE_ENV === "development") {
   peers = ["http://localhost:8765/gun"];
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV === "development") {
   peers = [];
 }
 
-const gun = new Gun({
+const gun = new GUN({
   peers,
 });
 
@@ -20,4 +20,4 @@ const gun = new Gun({
 const user = gun.user();
 
 export { gun };
-
+export { GUN };
