ff688ea486222c1ff53aabaf627101a8f7926e6e
Erik Holmgren <erikholmgren24@gmail.com>
2022-03-03 18:09:31 +0100

    Simple server to run aside the application.

---
+++ src/server.cjs
@@ -0,0 +1,29 @@
+// const GUN = require('gun');
+// const http = require('http');
+// 
+// const server = http.creteServer().listen(8765);
+// const gun = GUN({web: server});
+const fs = require('fs')
+const Gun = require('gun')
+
+function start() {
+  const config = {
+    port: process.env.GUNDB_PORT || 8765
+  }
+
+  if (process.env.HTTPS_KEY) {
+    config.key = fs.readFileSync(process.env.HTTPS_KEY)
+    config.cert = fs.readFileSync(process.env.HTTPS_CERT)
+    config.server = require('https').createServer(config, Gun.serve(__dirname))
+  } else {
+    config.server = require('http').createServer(Gun.serve(__dirname))
+  }
+
+  const gun = Gun({
+    web: config.server.listen(config.port)
+  })
+
+  console.log(`GunDB port: ${config.port}`)
+}
+
+start()
